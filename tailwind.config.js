1c0e71c690230fe0a2a68e320490ad7737667b9e
Nur <firoznur5@gmail.com>
2024-01-21 10:59:17 +0600

    navbar 1st stage done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //...
+    plugins: [require("daisyui")],
+  }
+  /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
